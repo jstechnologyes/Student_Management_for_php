ab02e31d15e955db7963d002276af6f79c1a0a84
Hadiuzzaman <hadi.mmpi@gmail.com>
2021-08-09 00:31:22 +0600

    First Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Govt. Primary School</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+<body class="jumbotron">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-sm-offset-4">
+				<table class="table table-bordered">
+				<tr>
+					<td>Student Information Portal</td>
+				</tr>
+				<tr>
+					<td>Choose</td>
+					<td></td>
+				</tr>
+			</table>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-3.5.1.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
