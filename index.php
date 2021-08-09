8d336cbb19eca4be2c335b68ac6ad8ae4c9f6ee2
Hadiuzzaman <hadi.mmpi@gmail.com>
2021-08-09 06:50:01 +0600

    sceond commit

--- index.php
+++ index.php
@@ -7,19 +7,44 @@
 </head>
 <body class="jumbotron">
 	<div class="container">
+		<a href="" class="btn btn-primary pull-right">Register</a>
+		<a href="" class="btn btn-primary pull-right">Login</a>
+		<h2 class="text-center">Govt Primary School</h2>
 		<div class="row">
-			<div class="col-sm-4 col-sm-offset-4">
+			<div class="col-sm-4" style="margin-left: 300px">
+				<form action="" method="post">
 				<table class="table table-bordered">
 				<tr>
-					<td>Student Information Portal</td>
+					<td colspan="2" class="text-center"><label>Student Information Portal</label></td>
 				</tr>
 				<tr>
-					<td>Choose</td>
-					<td></td>
+					<td><label>Choose</label></td>
+					<td>
+						<select class="form-control" required>
+							<option value="">Choose Class</option>
+							<option value="">Class One</option>
+							<option value="">Class Two</option>
+							<option value="">Class Three</option>
+							<option value="">Class Four</option>
+							<option value="">Class Five</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td><label>Roll No</label></td>
+					<td><input type="text" class="form-control" placeholder="Enter Roll No" required name=""></td>
+				</tr>
+				<tr>
+					<td colspan="2" class="text-center"><input type="submit" value="show info" class="btn btn-primary" name=""></td>
 				</tr>
 			</table>
+			</form>
 			</div>
 		</div>
+		<br><br><br>
+		<footer>
+			<p class="text-center">&copy; 2021- <?php echo date('Y'); ?> All Right Reserved.Govt Primary School</p>
+		</footer>
 	</div>
 
 
